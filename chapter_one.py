9947955115897acc44d79d6916d74f818c4b4eff
calvin p <cloth.sidle0i@icloud.com>
2023-12-11 21:06:04 -0500

    int varible += with float list value

--- chapter_one.py
+++ chapter_one.py
@@ -1382,4 +1382,20 @@ with open('/Users/calvinpineda/Downloads/mbox-short.txt','r') as file:
             print(float(line_split[1]))
 line_cal = sum(line_sum)/len(line_sum)
 print(line_cal)
+
+# updated to answer the specific exercise
+line_sum = []
+line_count = 0
+line_total = 0
+with open('/Users/calvinpineda/Downloads/mbox-short.txt','r') as file:
+    for x in file:
+        if x.startswith('X-DSPAM-Confidence:'):
+            line_count += 1
+            non_space = x.strip()
+            line_split = non_space.split(':')
+            line_sum.append(float(line_split[1]))
+            line_total += float(line_split[1]) # keep a running total
+line_cal = line_total/line_count
+print('Average spam confidence: ',line_cal)
+
     
--- playground-space.py
+++ playground-space.py
@@ -1,4 +1,13 @@
-cinput = int(input()) # input will be between -40 and 40
-
-
-
+line_sum = []
+line_count = 0
+line_total = 0
+with open('/Users/calvinpineda/Downloads/mbox-short.txt','r') as file:
+    for x in file:
+        if x.startswith('X-DSPAM-Confidence:'):
+            line_count += 1
+            non_space = x.strip()
+            line_split = non_space.split(':')
+            line_sum.append(float(line_split[1]))
+            line_total += float(line_split[1]) # keep a running total
+line_cal = line_total/line_count
+print('Average spam confidence: ',line_cal)
